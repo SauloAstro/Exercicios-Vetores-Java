0d0771480a73e313816cd106cb7771f940e6c1ff
SauloAstro <saulo.mozart09@gmail.com>
2023-09-07 01:03:33 -0300

    Primeiro Exercicio

---
+++ Exercicios-Vetores/Exercicio1/VetorA.java
@@ -0,0 +1,28 @@
+/*
+ 
+ Escreva um programa para ler um vetor A de 15 posições e imprimir seus elementos.
+ */
+
+
+public class VetorA {
+	
+	public static void main (String[] args) {
+		
+		int[] vetorA = new int[]{
+			
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+			
+			};
+		
+		
+		for(int i = 0;  i < vetorA.length; i++){
+			
+				System.out.println(vetorA[i]);
+			
+			
+			}
+		
+		
+	}
+}
+
